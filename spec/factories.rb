326d37aefe2a2c8836b42f61a8ae42bcc261e9c9
Kate Fulton <katesuzannefulton@gmail.com>
2015-01-26 11:57:26 -0800

    Create factories.rb file

---
+++ spec/factories.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :client do
+    first_name "John"
+    last_name  "Doe"
+    a_number "1234567"
+    nationality "Mexican"
+    gender "M"
+    represented "Yes"
+  end
+end
