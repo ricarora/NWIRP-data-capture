37b52c198ca1decabd822f8a0261d2555b23138d
Kate Fulton <katesuzannefulton@gmail.com>
2015-01-27 14:31:00 -0800

    Create conviction factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -7,4 +7,14 @@ FactoryGirl.define do
     gender "Male"
     represented true
   end
+
+  factory :conviction do
+    name "Assault 3"
+    rcw "9a.36.031"
+    sentence 365
+    ij_name "Judge Fitting"
+    nta_charges "212(a)(2)(A)(i)"
+    ij_decision_date "2015-01-27"
+    lj_finding "Vol Dept"
+  end
 end
