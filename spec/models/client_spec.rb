7d382d531fed5362eb5563ff22ba5b01ce3edc76
Richa Arora <ricarora@gmail.com>
2015-01-26 16:27:54 -0800

    add another test case

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe Client, :type => :model do
   let(:client) { FactoryGirl.create(:client) }
   describe ".validations" do
     it "must have a first name" do
-      client = FactoryGirl.create(:client, first_name: nil)
-      expect((client).valid?).to eq false
-      expect((:client).valid?).to eq true
+      invalid_client = Client.create(first_name: nil)
+      expect((invalid_client).valid?).to eq false
+      expect((client).valid?).to eq true
     end
   end
 end
