fbc7bd8b02c613620b3623a0a3ac405d88de5ad5
Kate Fulton <katesuzannefulton@gmail.com>
2015-01-26 16:07:17 -0800

    Fix removability grounds routing tests in specs

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Client, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryGirl.create(:user) }
+  describe ".validations" do
+    it "must have a first name" do
+      user = FactoryGirl.create(:user, first_name: nil)
+      expect((user).valid?).to eq false
+    end
+  end
 end
