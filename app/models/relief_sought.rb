deef3391988003fa4f49c87cd60cf8d68a0c10a5
Richa Arora <ricarora@gmail.com>
2015-01-28 14:07:52 -0800

    add validations for relief sought

--- app/models/relief_sought.rb
+++ app/models/relief_sought.rb
@@ -1,3 +1,5 @@
 class ReliefSought < ActiveRecord::Base
   self.primary_key = 'name'
+  validates :name, presence: true
+  # validates :name, uniqueness: { case_sensitive: false }
 end
--- spec/models/relief_sought_spec.rb
+++ spec/models/relief_sought_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe ReliefSought, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "name must be present" do
+    expect(ReliefSought.create(name: "").valid?).to eq false
+  end
+
+  it "name must be unique" do
+    expect(ReliefSought.create(name: "10 yrs COR").valid?).to eq true
+    expect(ReliefSought.create(name: "10 yrs COR").valid?).to eq false
+  end
+
+  it "name must be case insensitive" do
+    expect(ReliefSought.create(name: "10 yrs COR").valid?).to eq true
+    expect(ReliefSought.create(name: "10 yrs coR").valid?).to eq false
+  end
 end
