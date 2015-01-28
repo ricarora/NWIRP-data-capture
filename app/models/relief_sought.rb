df45ffd772269a442a7cee2d540dedb001881dba
Richa Arora <ricarora@gmail.com>
2015-01-28 14:10:00 -0800

    add space

--- app/models/relief_sought.rb
+++ app/models/relief_sought.rb
@@ -1,5 +1,5 @@
 class ReliefSought < ActiveRecord::Base
   self.primary_key = 'name'
   validates :name, presence: true
-  # validates :name, uniqueness: { case_sensitive: false }
+  validates :name, uniqueness: { case_sensitive: false }
 end
