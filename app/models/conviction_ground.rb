3f2b9dee675c25b6dd2938ff54b103398277431f
Richa Arora <ricarora@gmail.com>
2015-01-28 15:28:55 -0800

    add validations for convictoion ground

--- app/models/conviction_ground.rb
+++ app/models/conviction_ground.rb
@@ -1,3 +1,6 @@
 class ConvictionGround < ActiveRecord::Base
   has_many :grounds_for_removability, through: :conviction_ground
+  validates :status, inclusion: { in: %w(Yes No),
+    message: "Only accepts Yes, No or Finding not found."}
+  validates :gor_name, :uniqueness => { scope: [:conviction_id]}
 end
--- spec/models/conviction_ground_spec.rb
+++ spec/models/conviction_ground_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ConvictionGround, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "status can not be arbitrary" do
+    expect(ConvictionGround.create(gor_name: "DV", conviction_id: 4, status: "whatever").valid?).to eq false
+  end
+
+  it "gor_name & conviction id must be a unique pair" do
+    expect(ConvictionGround.create(gor_name: "DV", conviction_id: 4, status: "Yes").valid?).to eq true
+    expect(ConvictionGround.create(gor_name: "DV", conviction_id: 4, status: "Yes").valid?).to eq false
+  end
 end
