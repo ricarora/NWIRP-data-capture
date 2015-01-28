cd0d737b244521de57f0506cb89d543a91edc4d3
Kate Fulton <katesuzannefulton@gmail.com>
2015-01-28 14:20:46 -0800

    Write and pass test for unique combination for client relief model

--- app/models/client_relief.rb
+++ app/models/client_relief.rb
@@ -1,3 +1,4 @@
 class ClientRelief < ActiveRecord::Base
   validates :client_id, numericality: { only_integer: true }
+  validates :relief_name, :uniqueness => { scope: [:client_id]}
 end
--- spec/models/client_relief_spec.rb
+++ spec/models/client_relief_spec.rb
@@ -4,4 +4,19 @@ RSpec.describe ClientRelief, :type => :model do
   it "client_id must be an integer" do
     expect(ClientRelief.create(relief_name: "10 yr COR", client_id: "four").valid?).to eq false
   end
+
+  it "client_id and relief_name must be unique combination" do
+    expect(ClientRelief.create(relief_name: "10 yr COR", client_id: 4).valid?).to eq true
+    expect(ClientRelief.create(relief_name: "10 yr COR", client_id: 4).valid?).to eq false
+  end
+
+  it "client_id and relief_name must be unique combination" do
+    expect(ClientRelief.create(relief_name: "10 yr COR", client_id: 4).valid?).to eq true
+    expect(ClientRelief.create(relief_name: "10 yr COR", client_id: 5).valid?).to eq true
+  end
+
+  it "client_id and relief_name must be unique combination" do
+    expect(ClientRelief.create(relief_name: "LPR", client_id: 4).valid?).to eq true
+    expect(ClientRelief.create(relief_name: "10 yr COR", client_id: 4).valid?).to eq true
+  end
 end
