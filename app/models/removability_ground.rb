4a1db3e6831872413416983f4e54a195e41bb470
Richa Arora <ricarora@gmail.com>
2015-01-28 11:30:41 -0800

    add validations for removability ground

--- app/models/removability_ground.rb
+++ app/models/removability_ground.rb
@@ -1,3 +1,5 @@
 class RemovabilityGround < ActiveRecord::Base
   self.primary_key = 'name'
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+
 end
--- spec/models/removability_ground_spec.rb
+++ spec/models/removability_ground_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe RemovabilityGround, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:removability) { RemovabilityGround.create(:name => "DV") }
+  context "The removability ground has a name." do
+    it "name is present" do
+      removability.update(name: "")
+      expect((removability).valid?).to eq false
+    end
+    it "name is unique" do
+      expect((removability).valid?).to eq true
+      invalid_ground = RemovabilityGround.create(name: "DV")
+      expect((invalid_ground).valid?).to eq false
+    end
+
+    it "name is case insensitive" do
+      expect((removability).valid?).to eq true
+      invalid_ground = RemovabilityGround.create(name: "dv")
+      expect((invalid_ground).valid?).to eq false
+    end
+  end
 end
