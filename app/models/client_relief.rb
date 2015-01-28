e47a8e5720ece145767737bbb234d2f690fafdf7
Kate Fulton <katesuzannefulton@gmail.com>
2015-01-28 14:15:48 -0800

    Write and pass client relief test for client_id is integer

--- app/models/client_relief.rb
+++ app/models/client_relief.rb
@@ -1,2 +1,3 @@
 class ClientRelief < ActiveRecord::Base
+  validates :client_id, numericality: { only_integer: true }
 end
--- spec/models/client_relief_spec.rb
+++ spec/models/client_relief_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ClientRelief, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "client_id must be an integer" do
+    expect(ClientRelief.create(relief_name: "10 yr COR", client_id: "four").valid?).to eq false
+  end
 end
