7d4b9294c9f462cda2810aee5e5b3ddf151c645f
Richa Arora <ricarora@gmail.com>
2015-01-28 15:32:40 -0800

    add numericality validation to conviction ground

--- app/models/conviction_ground.rb
+++ app/models/conviction_ground.rb
@@ -1,6 +1,7 @@
 class ConvictionGround < ActiveRecord::Base
   has_many :grounds_for_removability, through: :conviction_ground
-  validates :status, inclusion: { in: %w(Yes No),
-    message: "Only accepts Yes, No or Finding not found."}
+  validates :status, inclusion: { in: ["Yes", "No", "Finding Not Found"],
+    message: "Only accepts Yes, No or Finding Not Found."}
   validates :gor_name, :uniqueness => { scope: [:conviction_id]}
+  validates :conviction_id, numericality: { only_integer: true }
 end
