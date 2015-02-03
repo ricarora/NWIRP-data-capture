017401b847c1964fe8c7560898ce1ed3f2d17455
Kate Fulton <katesuzannefulton@gmail.com>
2015-02-03 13:48:37 -0800

    Work on client save form

--- app/forms/client_build_form.rb
+++ app/forms/client_build_form.rb
@@ -5,18 +5,45 @@ class ClientBuildForm
   def initialize(attributes, client = nil)
     @attributes = attributes
     @client = client
+    @assessment = assessment
+    @relief = relief
     @errors = {}
   end
 
   def save
-    @client = Client.create(first_name: @attributes[:first_name],
-                            last_name: @attributes[:last_name],
-                            nationality: @attributes[:nationality],
-                            ethnicity: @attributes[:ethnicity],
-                            gender: @attributes[:gender],
-                            represented: @attributes[:represented],
-                            drru_case: @attributes[:drru_case],
-                            a_number: @attributes[:a_number])
+    @client = Client.new(first_name: @attributes[:first_name],
+                          last_name: @attributes[:last_name],
+                          nationality: @attributes[:nationality],
+                          ethnicity: @attributes[:ethnicity],
+                          gender: @attributes[:gender],
+                          represented: @attributes[:represented],
+                          drru_case: @attributes[:drru_case],
+                          a_number: @attributes[:a_number])
+    @client.valid?
+
+    if !@attributes[:assessment].empty?
+      @assessment = Assessment.new(date: Date.parse(@attributes[:assessment]))
+      @assessment.client = @client
+    end
+    @assessment.valid?
+
+    @attributes[:relief_sought].each do |name|
+      if ReliefSought.where(name: name).empty?
+        new_relief = ReliefSought.create(name: name)
+        add_client_relief(new_relief.name)
+      else
+        add_client_relief(ReliefSought.find(name).name)
+      end
+    end
+  end
+
+  def add_client_relief(name)
+    @relief = ClientRelief.new(relief_name: name)
+    @relief.client = @client
+    @relief.valid?
+  end
+
+
     if @client.errors.any?
       @errors[:client]=@client.errors
     end
@@ -40,7 +67,7 @@ class ClientBuildForm
   end
 
   def add_client_relief(name)
-    relief = ClientRelief.create(relief_name: name, client_id: @client.id)
+    relief = ClientRelief.new(relief_name: name, client_id: @client.id)
     client_errors(relief, :client_relief)
   end
 
@@ -52,5 +79,9 @@ class ClientBuildForm
         @errors[name_sym] = [name.errors]
       end
     end
+  end
+
+  def is_valid?
+    @client.valid?
   end
 end
