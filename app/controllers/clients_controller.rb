229a0bf50c9139835ff973205f9a7c338896703e
Kate Fulton <katesuzannefulton@gmail.com>
2015-02-07 11:06:59 -0800

    remove comments and json from clients controller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,29 +1,20 @@
 class ClientsController < ApplicationController
   before_action :set_client, only: [:show, :edit, :update, :destroy]
 
-  # GET /clients
-  # GET /clients.json
   def index
     @clients = Client.all
   end
 
-  # GET /clients/1
-  # GET /clients/1.json
   def show
   end
 
-  # GET /clients/new
   def new
-    # @client = Client.new()
     @client_form = ClientBuildForm.new
   end
 
-  # GET /clients/1/edit
   def edit
   end
 
-  # POST /clients
-  # POST /clients.json
   def create
     @client_form = ClientBuildForm.new
     if @client_form.submit(params[:client_build_form])
@@ -31,39 +22,19 @@ class ClientsController < ApplicationController
     else
       render :new
     end
-    # respond_to do |format|
-    #   if @client.save
-    #     format.html { redirect_to @client, notice: 'Client was successfully created.' }
-    #     format.json { render :show, status: :created, location: @client }
-    #   else
-    #     format.html { render :new }
-    #     format.json { render json: @client.errors, status: :unprocessable_entity }
-    #   end
-    # end
   end
 
-  # PATCH/PUT /clients/1
-  # PATCH/PUT /clients/1.json
   def update
-    respond_to do |format|
-      if @client.update(client_params)
-        format.html { redirect_to @client, notice: 'Client was successfully updated.' }
-        format.json { render :show, status: :ok, location: @client }
-      else
-        format.html { render :edit }
-        format.json { render json: @client.errors, status: :unprocessable_entity }
-      end
+    if @client.update(client_params)
+      redirect_to @client, notice: 'Client was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /clients/1
-  # DELETE /clients/1.json
   def destroy
     @client.destroy
-    respond_to do |format|
-      format.html { redirect_to clients_url, notice: 'Client was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to clients_url, notice: 'Client was successfully destroyed.'
   end
 
   private
@@ -71,9 +42,4 @@ class ClientsController < ApplicationController
     def set_client
       @client = Client.find(params[:id])
     end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    # def client_params
-    #   params.require(:client).permit(:last_name, :first_name, :nationality, :ethnicity, :gender, :represented, :drru_case, :a_number)
-    # end
 end
