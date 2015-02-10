04d4a37e2fe68f34e38f514f33e4520aba57537c
Richa Arora <ricarora@gmail.com>
2015-02-10 13:56:40 -0800

    add setup_search_form builder method in Application Helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def setup_search_form(builder)
+    fields = builder.grouping_fields builder.object.new_grouping,
+      object_name: 'new_object_name', child_index: "new_grouping" do |f|
+      render('grouping_fields', f: f)
+    end
+  end
 end
