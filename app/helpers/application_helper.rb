0eb48c899248c03a08d3db3074afa5524a7f0768
Richa Arora <ricarora@gmail.com>
2015-02-10 14:04:49 -0800

    add button to add & remove methods to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,16 @@ module ApplicationHelper
       render('grouping_fields', f: f)
     end
   end
+
+  def button_to_remove_fields(name, f)
+    content_tag :button, name, class: 'remove_fields'
+  end
+
+  def button_to_add_fields(name, f, type)
+    new_object = f.object.send "build_#{type}"
+    fields = f.send("#{type}_fields", new_object, child_index: "new_#{type}") do |builder|
+      render(type.to_s + "_fields", f: builder)
+    end
+    content_tag :button, name, :class => 'add_fields', 'data-field-type' => type, 'data-content' => "#{fields}"
+  end
 end
