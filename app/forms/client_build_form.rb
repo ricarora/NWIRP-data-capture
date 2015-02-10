51e3aecc2061cbe9c03beeb85877efa0d0bac5e9
Kate Fulton <katesuzannefulton@gmail.com>
2015-02-09 21:10:04 -0800

    work on client build form

--- app/forms/client_build_form.rb
+++ app/forms/client_build_form.rb
@@ -9,12 +9,12 @@ class ClientBuildForm
   validates :a_number, format: { with: /\d{3}-\d{3}-\d{3}/,
     message: "Only allows numbers in this format: XXX-XXX-XXX." }
   validates :gender, inclusion: { in: %w(Male Female Other Unknown),
-    message: "Only accepts Male, Female, Other, or Unknown."}
+    message: "Only accepts Male, Female, Other, or Unknown.", :allow_blank => true}
   validates :represented, inclusion: { in: %w(Yes No Unknown),
-    message: "Only accespts Yes, No, or Unknown."}
-  validates :drru_case, :inclusion => {:in => [true, false]}
-  validates :nationality, :inclusion => {:in => Client::NATIONALITY}
-  validates :nationality, :inclusion => {:in => Client::ETHNICITY}
+    message: "Only accepts Yes, No, or Unknown.", :allow_blank => true}
+  validates :drru_case, :inclusion => {:in => [true, false], :allow_blank => true}
+  validates :nationality, :inclusion => {:in => Client::NATIONALITY, allow_blank: true}
+  validates :nationality, :inclusion => {:in => Client::ETHNICITY, allow_blank: true}
 
 
   delegate :first_name, :last_name, :nationality, :ethnicity, :gender,
@@ -31,7 +31,6 @@ class ClientBuildForm
   end
 
   def submit(params)
-    #@relief_sought_array = []
     @client_relief_array = []
     client.last_name, client.first_name = params[:last_name], params[:first_name]
     client.nationality, client.ethnicity = params[:nationality], params[:ethnicity]
@@ -39,56 +38,48 @@ class ClientBuildForm
     client.represented = params[:represented]
     client.drru_case = params[:drru_case]
     client.a_number = params[:a_number]
-    unless params[:date].blank?
-      assessment.client = client
-      assessment.date = params[:date]
-    end
+    assessment.date = params[:date]
+    assessment.client = client
     params[:relief_name].each do |value|
       unless value.empty?
-        #relief_sought = ReliefSought.where(name: value).first_or_initialize
         client_relief = client.client_reliefs.build(relief_name: relief_sought.name)
-        #@relief_sought_array << relief_sought
         @client_relief_array << client_relief
       end
     end
     true
   end
 
-  def client_relief_valid?
-    if @client_relief_array.empty?
-      true
-    else
-      @client_relief_array.each do |client_relief|
-        if client_relief.valid?
-          true
-        else
-          @errors[:client_relief] += [@client_relief.errors]
-        end
-      end
-    end
-  end
-
-  # def relief_sought_valid?
-  #   unless @relief_sought_array.empty?
-  #     @relief_sought_array.each do |relief_sought|
-  #       relief_sought.valid?
+  # def client_relief_valid?
+  #   # unless @client_relief_array.empty?
+  #   #   @client_relief_array.each do |relief|
+  #   #     relief.valid? ? true : false
+  #   #   end
+  #   #   true
+  #   # end
+  #   if @client_relief_array.empty?
+  #     true
+  #   else
+  #     @client_relief_array.each do |client_relief|
+  #       if client_relief.valid?
+  #         true
+  #       else
+  #         @errors[:client_relief] += [@client_relief.errors]
+  #       end
   #     end
   #   end
-  #   true
+  # end
+  #
+  # def assessment_valid?
+  #   raise
+  #   assessment.valid?
   # end
 
-  def assessment_valid?
-    assessment.date.blank? ? true : assessment.valid?
-  end
-
   def all_valid?
+    raise
     valid?
   end
 
   def save
     client.save!
-    # @relief_sought_array.each do |relief_sought|
-    #   relief_sought.save!
-    # end
   end
 end
