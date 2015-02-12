f33eb05c408bd9a754426c1d661274f32002f41f
Richa Arora <ricarora@gmail.com>
2015-02-11 20:11:29 -0800

    rename relief num & max field variables

--- app/assets/javascripts/clients.js
+++ app/assets/javascripts/clients.js
@@ -4,14 +4,14 @@
 
 //functionality to add new fields to relief sought in new client form
 
-var num = 0;
+var relief_num = 0;
 
 function addReliefField(first) {
-  var max_field = 10;
-  if (num < max_field) {
-    num += 1;
+  var max_relief_field = 10;
+  if (relief_num < max_relief_field) {
+    relief_num += 1;
     var remove = first ? "</div>" : "<a href='#' class='remove_field'>Remove field</a></div>";
-    var emptyField = "<div> <select id='client_client_reliefs_attributes_0_relief_name' name=client[client_reliefs_attributes][" + (num - 1) + "][relief_name]><option value>Please select</option><option value='212C; WAIVER IN RMV PROCEED. PERMITTED BY CASE LAW'>212C; WAIVER IN RMV PROCEED. PERMITTED BY CASE LAW</option><option value='245 - ADJUSTMENT OF STATUS'>245 - ADJUSTMENT OF STATUS</option><option value='ASYLUM'>ASYLUM</option><option value='ASYLUM -WITHHOLDING'>ASYLUM -WITHHOLDING</option><option value='CHILD'>CHILD</option><option value='CITIZENSHIP'>CITIZENSHIP</option><option value='COERCIVE POPULATION CONTROL'>COERCIVE POPULATION CONTROL</option><option value='DACA'>DACA</option><option value='DAPA'>DAPA</option></select>";
+    var emptyField = "<div> <select id='client_client_reliefs_attributes_0_relief_name' name=client[client_reliefs_attributes][" + (relief_num - 1) + "][relief_name]><option value>Please select</option><option value='212C; WAIVER IN RMV PROCEED. PERMITTED BY CASE LAW'>212C; WAIVER IN RMV PROCEED. PERMITTED BY CASE LAW</option><option value='245 - ADJUSTMENT OF STATUS'>245 - ADJUSTMENT OF STATUS</option><option value='ASYLUM'>ASYLUM</option><option value='ASYLUM -WITHHOLDING'>ASYLUM -WITHHOLDING</option><option value='CHILD'>CHILD</option><option value='CITIZENSHIP'>CITIZENSHIP</option><option value='COERCIVE POPULATION CONTROL'>COERCIVE POPULATION CONTROL</option><option value='DACA'>DACA</option><option value='DAPA'>DAPA</option></select>";
     emptyField = emptyField + remove;
     $(".relief_field").append(emptyField);
   }
