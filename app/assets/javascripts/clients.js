2d73bd4cb8cf9a4c73aae6dcaa492720ba151262
Richa Arora <ricarora@gmail.com>
2015-02-12 11:32:27 -0800

    add data index attribute to relief sought field in client form javascript

--- app/assets/javascripts/clients.js
+++ app/assets/javascripts/clients.js
@@ -4,16 +4,21 @@
 
 //functionality to add new fields to relief sought in new client form
 
-var relief_num = 0;
+
 var assessment_num = 0;
 function addReliefField(first) {
-  var max_relief_field = 10;
-  if (relief_num < max_relief_field) {
-    relief_num += 1;
-    var remove = first ? "</div>" : "<a href='#' class='remove_field'>Remove field</a></div>";
-    var emptyField = "<div> <select id='client_client_reliefs_attributes_0_relief_name' name=client[client_reliefs_attributes][" + (relief_num - 1) + "][relief_name]><option value>Please select</option><option value='212C; WAIVER IN RMV PROCEED. PERMITTED BY CASE LAW'>212C; WAIVER IN RMV PROCEED. PERMITTED BY CASE LAW</option><option value='245 - ADJUSTMENT OF STATUS'>245 - ADJUSTMENT OF STATUS</option><option value='ASYLUM'>ASYLUM</option><option value='ASYLUM -WITHHOLDING'>ASYLUM -WITHHOLDING</option><option value='CHILD'>CHILD</option><option value='CITIZENSHIP'>CITIZENSHIP</option><option value='COERCIVE POPULATION CONTROL'>COERCIVE POPULATION CONTROL</option><option value='DACA'>DACA</option><option value='DAPA'>DAPA</option></select>";
-    emptyField = emptyField + remove;
-    $(".relief_field").append(emptyField);
+  var remove = first ? "</div>" : "<a href='#' class='remove_field'>Remove field</a></div>";
+  var emptyField = "<div> <select data-index="+ IndexNumber("relief_field") +" class='client_client_reliefs_attributes_0_relief_name' name=client[client_reliefs_attributes][" + IndexNumber("relief_field") + "][relief_name]><option value>Please select</option><option value='212C; WAIVER IN RMV PROCEED. PERMITTED BY CASE LAW'>212C; WAIVER IN RMV PROCEED. PERMITTED BY CASE LAW</option><option value='245 - ADJUSTMENT OF STATUS'>245 - ADJUSTMENT OF STATUS</option><option value='ASYLUM'>ASYLUM</option><option value='ASYLUM -WITHHOLDING'>ASYLUM -WITHHOLDING</option><option value='CHILD'>CHILD</option><option value='CITIZENSHIP'>CITIZENSHIP</option><option value='COERCIVE POPULATION CONTROL'>COERCIVE POPULATION CONTROL</option><option value='DACA'>DACA</option><option value='DAPA'>DAPA</option></select>";
+  emptyField = emptyField + remove;
+  $(".relief_field").append(emptyField);
+}
+
+function IndexNumber(class_name) {
+  $data = $("." + class_name + " select:last").data()
+  if ( $data != null || undefined) {
+    return $data.index + 1
+  } else {
+    return 0
   }
 }
 
