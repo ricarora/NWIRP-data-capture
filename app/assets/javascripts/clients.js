df8e830da6e2ba1fdbab6e224755f81ee5956f26
Richa Arora <ricarora@gmail.com>
2015-02-12 11:42:59 -0800

    add data index attribute to assessment field in client form javascript

--- app/assets/javascripts/clients.js
+++ app/assets/javascripts/clients.js
@@ -8,13 +8,13 @@
 var assessment_num = 0;
 function addReliefField(first) {
   var remove = first ? "</div>" : "<a href='#' class='remove_field'>Remove field</a></div>";
-  var emptyField = "<div> <select data-index="+ IndexNumber("relief_field") +" class='client_client_reliefs_attributes_0_relief_name' name=client[client_reliefs_attributes][" + IndexNumber("relief_field") + "][relief_name]><option value>Please select</option><option value='212C; WAIVER IN RMV PROCEED. PERMITTED BY CASE LAW'>212C; WAIVER IN RMV PROCEED. PERMITTED BY CASE LAW</option><option value='245 - ADJUSTMENT OF STATUS'>245 - ADJUSTMENT OF STATUS</option><option value='ASYLUM'>ASYLUM</option><option value='ASYLUM -WITHHOLDING'>ASYLUM -WITHHOLDING</option><option value='CHILD'>CHILD</option><option value='CITIZENSHIP'>CITIZENSHIP</option><option value='COERCIVE POPULATION CONTROL'>COERCIVE POPULATION CONTROL</option><option value='DACA'>DACA</option><option value='DAPA'>DAPA</option></select>";
+  var emptyField = "<div> <select data-index="+ IndexNumber("relief_field", "select") +" class='client_client_reliefs_attributes_0_relief_name' name=client[client_reliefs_attributes][" + IndexNumber("relief_field", "select") + "][relief_name]><option value>Please select</option><option value='212C; WAIVER IN RMV PROCEED. PERMITTED BY CASE LAW'>212C; WAIVER IN RMV PROCEED. PERMITTED BY CASE LAW</option><option value='245 - ADJUSTMENT OF STATUS'>245 - ADJUSTMENT OF STATUS</option><option value='ASYLUM'>ASYLUM</option><option value='ASYLUM -WITHHOLDING'>ASYLUM -WITHHOLDING</option><option value='CHILD'>CHILD</option><option value='CITIZENSHIP'>CITIZENSHIP</option><option value='COERCIVE POPULATION CONTROL'>COERCIVE POPULATION CONTROL</option><option value='DACA'>DACA</option><option value='DAPA'>DAPA</option></select>";
   emptyField = emptyField + remove;
   $(".relief_field").append(emptyField);
 }
 
-function IndexNumber(class_name) {
-  $data = $("." + class_name + " select:last").data()
+function IndexNumber(class_name, field_type) {
+  $data = $("." + class_name + " " + field_type + ":last").data()
   if ( $data != null || undefined) {
     return $data.index + 1
   } else {
@@ -23,14 +23,10 @@ function IndexNumber(class_name) {
 }
 
 function addAssessmentField(first) {
-  var max_assessment_field = 5;
-  if (assessment_num < max_assessment_field) {
-    assessment_num += 1;
-    var remove = first ? "</div>" : "<a href='#' class='remove_field'>Remove field</a></div>";
-    var emptyField = "<div><input id='client_assessments_attributes_0_date' name=client[assessments_attributes][" + (assessment_num - 1) + "][date] type='date'>";
-    emptyField = emptyField + remove;
-    $(".assessment_field").append(emptyField);
-  }
+  var remove = first ? "</div>" : "<a href='#' class='remove_field'>Remove field</a></div>";
+  var emptyField = "<div><input data-index=" + IndexNumber("assessment_field", "input") + " id='client_assessments_attributes_0_date' name=client[assessments_attributes][" + IndexNumber("assessment_field", "input") + "][date] type='date'>";
+  emptyField = emptyField + remove;
+  $(".assessment_field").append(emptyField);
 }
 
 
