69db5a5867cbcbd94f4f6c517077cd078fbc5290
Kate Fulton <katesuzannefulton@gmail.com>
2015-02-13 12:05:32 -0800

    add rcw to convictions database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150212220852) do
+ActiveRecord::Schema.define(version: 20150213200349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 20150212220852) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "state_committed"
+    t.string   "rcw"
   end
 
   create_table "relief_soughts", id: false, force: true do |t|
