642b30f89ae452fd971d5fdc3c759b8b55fd0a48
Kate Fulton <katesuzannefulton@gmail.com>
2015-02-13 08:49:42 -0800

    update removability ground tests

--- spec/models/removability_ground_spec.rb
+++ spec/models/removability_ground_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe RemovabilityGround, :type => :model do
-  let(:removability) { RemovabilityGround.create(:name => "DV") }
+  let(:removability) { RemovabilityGround.create(name: "DV") }
   context "The removability ground has a name." do
     it "name is present" do
       removability.update(name: "")
       expect((removability).valid?).to eq false
     end
-    
+
     it "name is unique" do
       expect((removability).valid?).to eq true
       invalid_ground = RemovabilityGround.create(name: "DV")
