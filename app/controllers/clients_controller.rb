dac6c72fa4c21ecccb385311a4423a708346b9e2
Richa Arora <ricarora@gmail.com>
2015-02-17 17:11:07 -0800

    add client & conviction choice in advance search - client controller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -74,13 +74,18 @@ class ClientsController < ApplicationController
   end
 
   def advanced_search
-    raise
-    @search = Client.search(params[:q])
-    @search.build_condition if @search.conditions.empty?
-    @clients  = params[:distinct].to_i.zero? ?
-      @search.result :
-      @search.result(distinct: true)
-
+    if params[:choice] == "client"
+      @search = Client.search(params[:q])
+      @search.build_condition if @search.conditions.empty?
+      @clients  = params[:distinct].to_i.zero? ?
+        @search.result :
+        @search.result(distinct: true)
+    else
+      @search = Conviction.search(params[:q])
+      @search.build_condition if @search.conditions.empty?
+      @convictions  = params[:distinct].to_i.zero? ?
+        @search.result :
+        @search.result(distinct: true)
   end
 
   private
