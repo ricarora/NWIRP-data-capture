feaf3839612a9c1ff00daeac70c53bf8359dd067
Richa Arora <ricarora@gmail.com>
2015-03-08 16:34:21 -0700

    work in progress - query save logic

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -9,11 +9,22 @@ class QueriesController < ApplicationController
           @search.result(distinct: true)
       else
         @search = Conviction.search(params[:q])
-        raise
+        if params[:query_save] && params[:query_name]
+          save_query(params[:query_name], @search)
+        end
         @search.build_condition if @search.conditions.empty?
         @convictions  = params[:distinct].to_i.zero? ?
           @search.result :
           @search.result(distinct: true)
       end
+  end
+
+  def save_query(name, search)
+    # checks if name & value both are present
+    if params[:q]["c"]["0"]["a"]["0"]["name"].length > 0 && params[:q]["c"]["0"]["v"]["0"]["value"].length > 0
+      @search.conditions.present?
+      q = Query.new(name: name, description: search)
+      q.save
+    end
   end
 end
