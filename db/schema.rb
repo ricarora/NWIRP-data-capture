9114d15d96c30ff32c218842be4fc25cabb616d1
Kate Fulton <katesuzannefulton@gmail.com>
2015-03-17 19:04:31 -0700

    add middle name field to client

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150213201443) do
+ActiveRecord::Schema.define(version: 20150318020308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20150213201443) do
     t.string   "encrypted_a_number"
     t.string   "encrypted_a_number_salt"
     t.string   "encrypted_a_number_iv"
+    t.string   "middle_name"
   end
 
   create_table "conviction_grounds", force: true do |t|
@@ -88,12 +89,12 @@ ActiveRecord::Schema.define(version: 20150213201443) do
   add_index "removability_grounds", ["name"], name: "index_removability_grounds_on_name", unique: true, using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -102,8 +103,10 @@ ActiveRecord::Schema.define(version: 20150213201443) do
     t.datetime "updated_at"
     t.boolean  "admin"
     t.string   "name"
+    t.boolean  "approved",               default: false, null: false
   end
 
+  add_index "users", ["approved"], name: "index_users_on_approved", using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
