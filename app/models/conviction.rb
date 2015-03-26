da1c0bf891dfb1e0a0dc5f6878a753812a9e8952
Richa Arora <ricarora@gmail.com>
2015-03-25 17:11:30 -0700

    try deploy 2

--- app/models/conviction.rb
+++ app/models/conviction.rb
@@ -29,7 +29,7 @@ class Conviction < ActiveRecord::Base
     "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee",
     "Texas", "Utah", "Vermont", "Virginia", "West Virginia", "Wisconsin", "Wyoming"]
 
-  validates :ij_name, :inclusion => {:in => Conviction::IJ_NAME, allow_blank: true}
+  validates :ij_name, :inclusion => {:in => ["Odell", "Scala", "Fitting"], allow_blank: true}
   validates :state_committed, :inclusion => {:in => Conviction::STATE_COMMITTED, allow_blank: true}
 
   def ij_decision_date_is_date?
