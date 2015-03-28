717961086de4a3b15d7037b80b2317c4f6f23aab
Richa Arora <ricarora@gmail.com>
2015-03-28 16:08:40 -0700

    update devise host in development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'nwirp-db.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
   # config.action_mailer.delivery_method = :smtp
  #  config.action_mailer.smtp_settings = {
  #   address:              'smtp.mandrillapp.com',
