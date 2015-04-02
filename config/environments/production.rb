41667fc24fd196d55b54ed71cff50f21eb6e84d2
Kate Fulton <katesuzannefulton@gmail.com>
2015-04-02 07:58:56 -0700

    add mandrill mailing information

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,11 +80,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'nwirp-db.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
-    :address    => "smtp.sendgrid.net",
-    :port       => 25,
-    :user_name  => ENV['SENDGRID_USERNAME'],
-    :password   => ENV['SENDGRID_PASSWORD'],
-    :domain     => ENV['SENDGRID_DOMAIN'],
-    :authentication  => :plain
+      :port =>           '587',
+      :address =>        'smtp.mandrillapp.com',
+      :user_name =>      ENV['MANDRILL_USERNAME'],
+      :password =>       ENV['MANDRILL_APIKEY'],
+      :domain =>         'heroku.com',
+      :authentication => :plain
   }
+  ActionMailer::Base.delivery_method = :smtp
 end
