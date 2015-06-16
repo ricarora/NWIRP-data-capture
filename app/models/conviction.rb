ef305b3fca17abd0dfc1ac8c1feed79cbe9f1b83
Richa Arora <ricarora@gmail.com>
2015-06-16 13:18:14 -0700

    add federal to states dropdown

--- app/models/conviction.rb
+++ app/models/conviction.rb
@@ -20,12 +20,12 @@ class Conviction < ActiveRecord::Base
 
   IJ_NAME = Judge.judge_arr
 
-  STATE_COMMITTED = ["Washington", "Alabama", "Alaska", "Arizona", "Arkansas", "California",
+  STATE_COMMITTED = ["Washington", "Oregon", "Federal", "Alabama", "Alaska", "Arizona", "Arkansas", "California",
     "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho",
     "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine",
     "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri",
     "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico",
-    "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon",
+    "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma",
     "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee",
     "Texas", "Utah", "Vermont", "Virginia", "West Virginia", "Wisconsin", "Wyoming"]
 
